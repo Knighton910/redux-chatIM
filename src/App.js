fbb7626b143afc30c234f44548773f55d408c216
lordKnighton <keltheceo@gmail.com>
2019-11-29 11:49:12 -0600

    initial for Chat_intermediate

---
+++ src/App.js
@@ -0,0 +1,140 @@
+import React from 'react';
+import uuid from 'uuid';
+import { createStore } from 'redux';
+
+function reducer(state, action) {
+  if (action.type === 'ADD_MESSAGE') {
+    const newMessage = {
+      text: action.text,
+      timestamp: Date.now(),
+      id: uuid.v4(),
+    };
+    return {
+      messages: state.messages.concat(newMessage),
+    };
+  } else if (action.type === 'DELETE_MESSAGE') {
+    return {
+      messages: state.messages.filter((m) => (
+        m.id !== action.id
+      ))
+    };
+  } else {
+    return state;
+  }
+}
+
+const initialState = {
+  activeThreadId: '1-fca2',
+  threads: [
+    // Two threads in state
+    {
+      id: '1-fca2',
+      title: 'Buzz Aldrin',
+      messages: [
+        {
+          text: 'Twelve minutes to ignition.',
+          timestamp: Date.now(),
+          id: uuid.v4(),
+        },
+      ],
+    },
+    {
+      id: '2-be91',
+      title: 'Micheal Collins',
+      messages: [],
+    },
+  ],
+};
+
+const store = createStore(reducer, initialState);
+
+class App extends React.Component {
+  componentDidMount() {
+    store.subscribe(() => this.forceUpdate());
+  }
+
+  render() {
+    const messages = store.getState().messages;
+
+    return (
+      <div className='ui segment'>
+        <MessageView messages={messages} />
+        <MessageInput />
+      </div>
+    );
+  }
+}
+
+class MessageInput extends React.Component {
+  state = {
+    value: '',
+  };
+
+  onChange = (e) => {
+    this.setState({
+      value: e.target.value,
+    })
+  };
+
+  handleSubmit = () => {
+    store.dispatch({
+      type: 'ADD_MESSAGE',
+      text: this.state.value,
+    });
+    this.setState({
+      value: '',
+    });
+  };
+
+  render() {
+    return (
+      <div className='ui input'>
+        <input
+          onChange={this.onChange}
+          value={this.state.value}
+          type='text'
+        />
+        <button
+          onClick={this.handleSubmit}
+          className='ui primary button'
+          type='submit'
+        >
+          Submit
+        </button>
+      </div>
+    );
+  }
+}
+
+class MessageView extends React.Component {
+  handleClick = (id) => {
+    store.dispatch({
+      type: 'DELETE_MESSAGE',
+      id: id,
+    });
+  };
+
+  render() {
+    const messages = this.props.messages.map((message, index) => (
+      <div
+        className='comment'
+        key={index}
+        onClick={() => this.handleClick(message.id)} // Use `id`
+      >
+        <div className='text'> {/* Wrap message data in `div` */}
+          {message.text}
+          <span className='metadata'>@{message.timestamp}</span>
+        </div>
+      </div>
+    ));
+    return (
+      <div className='ui center aligned basic segment'>
+        <div className='ui comments'>
+          {messages}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
